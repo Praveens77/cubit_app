fd800a3f4dafdfb04406ae010e721b58d087dc83
Praveen Chandra <pcps72001@gmail.com>
2023-12-24 14:46:05 +0530

    api part done in cubit

---
+++ lib/splash/splash_controller.dart
@@ -0,0 +1,8 @@
+import 'dart:async';
+
+class SplashController {
+  Future<void> initialize() async {
+    // Simulate a delay of 3 seconds
+    await Future.delayed(const Duration(seconds: 3));
+  }
+}
